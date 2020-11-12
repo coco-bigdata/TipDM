2ccc2339a3adcef6cd9c0de2534949ac2b0b5ce3
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-11-12 18:02:18 +0800

    update

--- backend/tipdm-server/src/main/resources/sql/quartz_postgres.sql
+++ backend/tipdm-server/src/main/resources/sql/quartz_postgres.sql
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-version https://git-lfs.github.com/spec/v1
-oid sha256:e726e38dc98bda913507d23b23cffc0caf16d32f145bd81e5dfd5c9354a17cb9
-size 6808
-=======
 -- Thanks to Patrick Lightbody for submitting this...
 --
 -- In your Quartz properties file, you'll need to set 
